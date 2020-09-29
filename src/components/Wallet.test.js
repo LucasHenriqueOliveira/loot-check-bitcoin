28869023eb9f43ea07105190b4d825e44f8a67ce
Lucas <lucashen@gmail.com>
2020-09-29 08:08:20 -0300

    test and build a wallet component

---
+++ src/components/Wallet.test.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { Wallet } from './Wallet';
+
+describe('Wallet', () => {
+    const props = { balance: 20 };
+    const wallet = shallow(<Wallet {...props} />);
+
+    it('Renders properly', () => {
+        expect(wallet).toMatchSnapshot();
+    });
+
+    it('Displays the balance from props', () => {
+        expect(wallet.find('.balance').text()).toEqual('Wallet balance: 20');
+    });
+});
