1df52db9f4865e30717cc1db86d2280e443e8cc0
Lucas <lucashen@gmail.com>
2020-10-02 08:06:49 -0300

    support redux deposit and withdraw methods

--- src/actions/balance.test.js
+++ src/actions/balance.test.js
@@ -8,3 +8,17 @@ it('Creates an action to set the balance', () => {
 
     expect(actions.setBalance(balance)).toEqual(expectedAction);
 });
+
+it('Creates an action to deposit into the balance', () => {
+    const deposit = 10;
+    const expectedAction = { type: constants.DEPOSIT, deposit };
+
+    expect(actions.deposit(deposit)).toEqual(expectedAction);
+});
+
+it('Creates an action to withdraw from the balance', () => {
+    const withdraw = 10;
+    const expectedAction = { type: constants.WITHDRAW, withdraw };
+
+    expect(actions.withdraw(withdraw)).toEqual(expectedAction);
+});
--- src/reducers/balance.test.js
+++ src/reducers/balance.test.js
@@ -7,4 +7,18 @@ describe('Balance Reducer', () => {
         
         expect(balanceReducer(undefined, { type: constants.SET_BALANCE, balance})).toEqual(balance);
     });
+
+    it('Deposits into the balance', () => {
+        const deposit = 10;
+        const initialState = 5;
+
+        expect(balanceReducer(initialState, { type: constants.DEPOSIT, deposit})).toEqual(initialState + deposit);
+    });
+
+    it('Withdraws from the balance', () => {
+        const withdraw = 10;
+        const initialState = 15;
+
+        expect(balanceReducer(initialState, { type: constants.WITHDRAW, withdraw})).toEqual(initialState - withdraw);
+    });
 });
